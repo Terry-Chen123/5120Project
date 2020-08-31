59139b8e2c20ddebe3e5ebd95cc748122de11d3d
Terry <tche0039@student.monash.edu>
2020-09-01 02:07:49 +1000

    update

---
+++ static/css/pop-up.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+function closeWindow(){
+	$('.popup_back,.popup_window_first').hide();
+}
+$('.btn_click').click(function(){
+	$('.popup_window_first,.popup_back').show();
+
+});
