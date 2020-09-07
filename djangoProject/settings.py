d58386a1cb6f2aa2c521ff297b7a7afe6234e5cd
Terry <tche0039@student.monash.edu>
2020-09-08 00:08:57 +1000

    21
    12323

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -84,10 +84,10 @@ DATABASES = {
     {
         'ENGINE': 'django.db.backends.mysql',    # 数据库引擎
         'NAME': '5120db', # 数据库名称
-        'HOST': 'localhost', # 数据库地址，本机 ip 地址 127.0.0.1
+        'HOST': 'lunghealth.mysql.database.azure.com', # 数据库地址，本机 ip 地址 127.0.0.1
         'PORT': '3306', # 端口
-        'USER': 'root',  # 数据库用户名
-        'PASSWORD': 'qwer123567', # 数据库密码
+        'USER': 'pinnacle@lunghealth',  # 数据库用户名
+        'PASSWORD': 'Qwer123567', # 数据库密码
 
     }
 }
